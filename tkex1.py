1a3f3d57ef0c7d87edadb4e367336e7fc5bffee2
ty-hongjy <hongjy1@163.com>
2016-09-25 20:40:34 +0800

    0924

--- tkex1.py
+++ tkex1.py
@@ -19,7 +19,27 @@ def hit_me():
 		var.set("")
 		on_hit=False
 
+def insert_point():
+	var=e.get()
+	t.insert("insert",var)
+
+def insert_end():
+	var=e.get()
+	t.insert("end",var)
+
 b=tk.Button(window,text="hit me",width=15,height=2,command=hit_me)
 b.pack()
+
+e=tk.Entry(window,show=None)
+e.pack()
+
+b1=tk.Button(window,text="insert point",width=15,height=2,command=insert_point)
+b1.pack()
+
+b2=tk.Button(window,text="insert end",width=15,height=2,command=insert_end)
+b2.pack()
+
+t=tk.Text(window,height=2)
+t.pack()
 
 window.mainloop()
