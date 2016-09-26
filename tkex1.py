c28a7eba78645a96918e2594432561585e8503e6
ty-honjy <hongjy1@163.com>
2016-09-26 15:00:54 +0800

    0926

--- tkex1.py
+++ tkex1.py
@@ -6,7 +6,7 @@ window.geometry("200x100")
 
 var=tk.StringVar()
 #l=tk.Label(window,textvariable=var ,bg="green",font=("Arial",12),width=15,height=12)
-l=tk.Label(window,textvariable=var ,bg="green",font=("Arial",12),width=15,height=12)
+l=tk.Label(window,textvariable=var ,bg="green",font=("Arial",12),width=15,height=2)
 l.pack()
 
 on_hit=False
@@ -27,19 +27,42 @@ def insert_end():
 	var=e.get()
 	t.insert("end",var)
 
-b=tk.Button(window,text="hit me",width=15,height=2,command=hit_me)
-b.pack()
+def show_sel():
+	v1=lb.get(lb.curselection())
+	var.set(v1)
 
-e=tk.Entry(window,show=None)
-e.pack()
+#b=tk.Button(window,text="hit me",width=15,height=2,command=hit_me)
+#b.pack()
 
-b1=tk.Button(window,text="insert point",width=15,height=2,command=insert_point)
-b1.pack()
+#e=tk.Entry(window,show=None)
+#e.pack()
 
-b2=tk.Button(window,text="insert end",width=15,height=2,command=insert_end)
-b2.pack()
+#b1=tk.Button(window,text="insert point",width=15,height=2,command=insert_point)
+#b1.pack()
+
+#b2=tk.Button(window,text="insert end",width=15,height=2,command=insert_end)
+#b2.pack()
 
 t=tk.Text(window,height=2)
 t.pack()
+
+
+#################################################
+
+var2=tk.StringVar()
+var2.set((11,22,33,44))
+lb=tk.Listbox(window,listvariable=var2)
+list_items=[1,2,3,4]
+for item in list_items:
+	lb.insert("end",item)
+
+
+lb.insert(1,"first")
+lb.insert(2,"secnd")
+lb.delete(5)
+lb.pack()
+
+b3=tk.Button(window,text="show selection",width=15,height=2,command=show_sel)
+b3.pack()
 
 window.mainloop()
