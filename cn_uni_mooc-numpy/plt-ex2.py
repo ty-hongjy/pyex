cba6814863f5971ccbe64757fba6227aacbcfe2c
ty-hongjy <hongjy1@163.com>
2017-04-24 14:32:42 +0800

    0424

---
+++ cn_uni_mooc-numpy/plt-ex2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 20 11:52:05 2017
+
+@author: hongjy
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+ 
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+ 
+a = np.arange(0.0, 5.0, 0.02)
+ 
+plt.subplot(2,1,1)
+plt.plot(a,f(a))
+ 
+plt.subplot(2,1,2)
+plt.plot(a, np.cos(np.pi*a), 'r--')
+plt.show()
