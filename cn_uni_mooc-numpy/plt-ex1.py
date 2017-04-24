74d8a1d62f937979906b2b37b0631785e0537058
ty-hongjy <hongjy1@163.com>
2017-04-24 14:30:52 +0800

    0424

---
+++ cn_uni_mooc-numpy/plt-ex1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+labels='Frogs','Dogs','Hogs','Logs'
+sizes=[15,30,45,30]
+explode=[0,0.05,0,0]
+#plt.subplot(2,2,1)
+plt.figure()
+plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',startangle=90)
+plt.axis('equal')
+#plt.show()
+
+np.random.seed(0)
+mu,sigma=0,1
+a=np.random.normal(mu,sigma,size=1000)
+#plt.subplot(2,2,2)
+plt.figure()
+plt.hist(a,30,normed=1,histtype='stepfilled')
+#plt.show()
+
+#plt.subplot(2,2,4)
+plt.figure()
+plt.plot(10*np.random.randn(1000),10*np.random.randn(1000),'ro')
+plt.title('Simple Scatter')
+plt.show()
