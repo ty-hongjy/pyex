dba95e18aa611f782beb7b9d5dd79c1483b27ab8
ty-hongjy <hongjy1@163.com>
2017-06-06 11:51:23 +0800

    0606

--- cn_uni_mooc-sklearn/test/test1.py
+++ cn_uni_mooc-sklearn/test/test1.py
@@ -5,13 +5,6 @@ Created on Tue Jun  6 11:05:05 2017
 @author: hongjy
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 24 13:53:31 2017
-
-@author: hongjy
-"""
-
 import numpy as np
 from sklearn.cluster import KMeans
 
@@ -21,6 +14,6 @@ if __name__ == '__main__':
     label = km.fit_predict(data)
     print(label)
     print(km.cluster_centers_)
-
+    
     expenses = np.sum(km.cluster_centers_,axis=1)
     print(expenses)
