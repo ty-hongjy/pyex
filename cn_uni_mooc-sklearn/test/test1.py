82ec80d475d556ef419b78cd4ba2061dda289624
ty-hongjy <hongjy1@163.com>
2017-06-06 11:49:50 +0800

    0606

--- cn_uni_mooc-sklearn/test/test1.py
+++ cn_uni_mooc-sklearn/test/test1.py
@@ -16,7 +16,6 @@ import numpy as np
 from sklearn.cluster import KMeans
 
 if __name__ == '__main__':
-    #data= ((3,1), (3,2), (4,1), (4,2) (1,3) (1,4) (2,3) (2,4))
     data= ((3,1), (3,2), (4,1), (4,2), (1,3), (1,4), (2,3), (2,4))
     km = KMeans(n_clusters=2)
     label = km.fit_predict(data)
@@ -25,4 +24,3 @@ if __name__ == '__main__':
 
     expenses = np.sum(km.cluster_centers_,axis=1)
     print(expenses)
-    CityCluster = [[],[],[],[]]
