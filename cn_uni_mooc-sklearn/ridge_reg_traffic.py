acd24fca78178d54cc78e5dd19c71ecbe34469b7
ty-hongjy <hongjy1@163.com>
2017-06-07 17:54:21 +0800

    0607

--- cn_uni_mooc-sklearn/ridge_reg_traffic.py
+++ cn_uni_mooc-sklearn/ridge_reg_traffic.py
@@ -19,23 +19,23 @@ def main():
     y = data[:,5]
     poly = PolynomialFeatures(9)
     x = poly.fit_transform(x)
-    
-    train_x, test_x, train_y, test_y = \
-    cross_validation.train_test_split(x, y, 
-    test_size=0.3, random_state=0)
-    clf = Ridge(alpha=1.0, fit_intercept=True)
-    clf.fit(train_x, train_y)
-    score = clf.score(test_x, test_y)
-    print('score:',score)
-    
-    start = 200
-    end = 300
-    y_pre = clf.predict(x)
-    time = np.arange(start, end)
-    plt.plot(time, y[start:end],'b', label="real")
-    plt.plot(time, y_pre[start:end],'r', label="predict")
-    plt.legend(loc="upper left")
-    plt.show()
+#    
+#    train_x, test_x, train_y, test_y = \
+#    cross_validation.train_test_split(x, y, 
+#    test_size=0.3, random_state=0)
+#    clf = Ridge(alpha=1.0, fit_intercept=True)
+#    clf.fit(train_x, train_y)
+#    score = clf.score(test_x, test_y)
+#    print('score:',score)
+#    
+#    start = 200
+#    end = 300
+#    y_pre = clf.predict(x)
+#    time = np.arange(start, end)
+#    plt.plot(time, y[start:end],'b', label="real")
+#    plt.plot(time, y_pre[start:end],'r', label="predict")
+#    plt.legend(loc="upper left")
+#    plt.show()
  
 if __name__ == '__main__':
     main()
