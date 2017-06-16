b88d4296aee80a39e5074854caac4a2fb86da4b4
ty-hongjy <hongjy1@163.com>
2017-06-16 16:46:19 +0800

    0616

--- cn_uni_mooc-sklearn/test/knn_dt_nb_test1.py
+++ cn_uni_mooc-sklearn/test/knn_dt_nb_test1.py
@@ -8,8 +8,6 @@ import time
 import pandas as pd
 import numpy as np  
  
-#from sklearn.cluster import KMeans
-
 from sklearn.preprocessing import Imputer
 from sklearn.cross_validation import train_test_split 
 from sklearn.metrics import classification_report
@@ -18,9 +16,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 
-ISOTIMEFORMAT='%Y-%m-%d %X' #add by me
-_is_verify=False
-#_is_verify=True
+ISOTIMEFORMAT='%Y-%m-%d %X'
+#_is_verify=False
+_is_verify=True
  
 def load_datasets(filepaths):
 #def load_datasets(filepaths, label_paths):
@@ -28,26 +26,20 @@ def load_datasets(filepaths):
     print(time.strftime(ISOTIMEFORMAT,time.localtime(time.time()))) #add by me
     feature = np.ndarray(shape=(0,54))
     label = np.ndarray(shape=(0,1))
-#    for file in feature_paths:
     df1 = pd.read_table(filepaths, delimiter=' ', na_values='?', header=None)
     if len(df1.columns)==55:
         df=df1.drop(54,axis=1)
     else:
         df=df1
-#    print(len(df.columns))
+
     imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
     imp.fit(df)
     df = imp.transform(df)
     feature = np.concatenate((feature, df))
-#    print(feature.shape) 
-#    for file in label_paths:
-#        df = pd.read_table(file, header=None)
-#        label = np.concatenate((label, df))
 
     if len(df1.columns)==55:         
 #    if type=='train':
         label = df1[54]
-#        print(label.shape)
         print("end loaded data")
         print(time.strftime(ISOTIMEFORMAT,time.localtime(time.time()))) #add by me
         return feature, label
@@ -63,15 +55,11 @@ def main():
     testfilePaths = 'test_data\data_test.txt'
     ''' 读入数据  '''
     x,y = load_datasets(trainfilePaths)
-#    x_train,y_train = load_datasets(trainfilePaths)
-#    x_train, x_, y_train, y_ = train_test_split(x_train, y_train, test_size = 0.0)
 #step 1 take x and y as train_data and test_data
     if _is_verify:
         print('Step 1')
         x_train, x_test, y_train, y_test = \
             train_test_split(x, y, test_size=0.3, random_state=0)
-    #    x_train, x_test, y_train, y_test = \
-    #        cross_validation.train_test_split(x, y, test_size=0.3, random_state=0)
         print('Start knn training')
         knn = KNeighborsClassifier().fit(x_train, y_train)
         print('Training done')
@@ -80,7 +68,6 @@ def main():
         print('Start knn predict')
         answer_knn = knn.predict(x_test)
         print(len(answer_knn))
-    #    pd.write_table(answer_knn,'module2.txt', delimiter=' ', na_values='?')
         print('Prediction done')
         print(time.strftime(ISOTIMEFORMAT,time.localtime(time.time())))
     #    return  None     
@@ -89,7 +76,6 @@ def main():
         dt = DecisionTreeClassifier().fit(x_train, y_train)
         print('Training done')
         answer_dt = dt.predict(x_test)
-    #    pd.write_table(answer_dt,'module2.txt', delimiter=' ', na_values='?')
         print('Prediction done')
         print(time.strftime(ISOTIMEFORMAT,time.localtime(time.time())))
          
@@ -97,7 +83,6 @@ def main():
         gnb = GaussianNB().fit(x_train, y_train)
         print('Training done')
         answer_gnb = gnb.predict(x_test)
-    #    pd.write_table(answer_gnb,'module3.txt', delimiter=' ', na_values='?')
         print('Prediction done')
         print(time.strftime(ISOTIMEFORMAT,time.localtime(time.time())))
           
