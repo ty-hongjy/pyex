cf4c470137f86892eecbfe04ff1b162764dd1052
jiangyong hong <jiangyong hong>
2017-07-13 21:27:15 +0800

    0713

---
+++ cn_uni_mooc-3d/SV06V01_dragon.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 13 21:04:34 2017
+
+@author: Administrator
+"""
+
+from mayavi import mlab
+from os.path import join
+#from os import path
+import os
+import tarfile
+ 
+#读取tar压缩文件
+dragon_tar_file = tarfile.open('dragon.tar.gz')
+try:
+    os.mkdir('dragon_data')
+except:
+    pass
+dragon_tar_file.extractall('dragon_data')
+dragon_tar_file.close()
+dragon_ply_file = join('dragon_data', 'dragon_recon', 'dragon_vrip.ply')
+ 
+# 渲染dragon ply文件
+mlab.pipeline.surface(mlab.pipeline.open(dragon_ply_file))
+mlab.show()
+ 
+#删除解压的文件夹
+import shutil
+shutil.rmtree('dragon_data')
