3d17e67ae3c8519ca8e939a8cc8c1e34d41ae9d1
ty-hongjy <hongjy1@163.com>
2017-07-17 17:32:28 +0800

    0717

--- cn_uni_mooc-3d/SV05V07_vector.py
+++ cn_uni_mooc-3d/SV05V07_vector.py
@@ -6,23 +6,26 @@ Created on Wed Jul  5 21:01:10 2017
 """
 import numpy as np
 from mayavi import mlab
+from mayavi.tools import pipeline
 
-#_MODE_='normal'
+_MODE_='normal'
 #_MODE_='mask'
 #_MODE_='flow'
 #_MODE_='iso'
-_MODE_='cut'
+#_MODE_='cut'
+#_MODE_='comp'
 
 x, y, z = np.mgrid[0:1:20j, 0:1:20j, 0:1:20j]
 u = np.sin(np.pi*x) * np.cos(np.pi*z)
 v = -2*np.sin(np.pi*y) * np.cos(2*np.pi*z)
 w = np.cos(np.pi*x)*np.sin(np.pi*z) + np.cos(np.pi*y)*np.sin(2*np.pi*z)
 
+print("ok")
 src=mlab.pipeline.vector_field(u,v,w)
 
 if _MODE_=='mask':
     mlab.pipeline.vectors(src,mask_points=10,scale_factor=2.0) 
-
+#    mlab.quiver3d(u,v,w)
 elif _MODE_=='cut':
     mlab.pipeline.vector_cut_plane(src,mask_points=10,scale_factor=2.0) 
 
@@ -36,9 +39,10 @@ elif _MODE_=='flow':
               integration_direction='both')
     mlab.outline()
 
+elif _MODE_=='comp':
+    pass
 else:
     mlab.quiver3d(u,v,w)
     mlab.outline()
 
-#mlab.quiver3d(u,v,w)
 mlab.show()
