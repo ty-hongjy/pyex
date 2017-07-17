8817de36be2490c80f4eecc0e7adf6c71d28c95a
ty-hongjy <hongjy1@163.com>
2017-07-17 17:21:11 +0800

    0717

--- cn_uni_mooc-3d/SV05V07_vector.py
+++ cn_uni_mooc-3d/SV05V07_vector.py
@@ -4,15 +4,41 @@ Created on Wed Jul  5 21:01:10 2017
 
 @author: Administrator
 """
-
 import numpy as np
+from mayavi import mlab
+
+#_MODE_='normal'
+#_MODE_='mask'
+#_MODE_='flow'
+#_MODE_='iso'
+_MODE_='cut'
+
 x, y, z = np.mgrid[0:1:20j, 0:1:20j, 0:1:20j]
-u =    np.sin(np.pi*x) * np.cos(np.pi*z)
+u = np.sin(np.pi*x) * np.cos(np.pi*z)
 v = -2*np.sin(np.pi*y) * np.cos(2*np.pi*z)
 w = np.cos(np.pi*x)*np.sin(np.pi*z) + np.cos(np.pi*y)*np.sin(2*np.pi*z)
- 
-from mayavi import mlab
-mlab.quiver3d(u,v,w)
-mlab.outline()
- 
+
+src=mlab.pipeline.vector_field(u,v,w)
+
+if _MODE_=='mask':
+    mlab.pipeline.vectors(src,mask_points=10,scale_factor=2.0) 
+
+elif _MODE_=='cut':
+    mlab.pipeline.vector_cut_plane(src,mask_points=10,scale_factor=2.0) 
+
+elif _MODE_=='iso':
+    magnitude=mlab.pipeline.extract_vector_norm(src) 
+    mlab.pipeline.iso_surface(magnitude,contours=[2.0,0.5])
+    mlab.outline()
+
+elif _MODE_=='flow':
+    mlab.flow(u,v,w,seed_scale=1,seed_resolution=5,
+              integration_direction='both')
+    mlab.outline()
+
+else:
+    mlab.quiver3d(u,v,w)
+    mlab.outline()
+
+#mlab.quiver3d(u,v,w)
 mlab.show()
