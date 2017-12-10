1ead49126e17f57c0fa38c0e355f992820a72505
ty-hongjy <hongjy1@163.com>
2017-12-10 14:23:16 +0800

    1210

--- noip/py2017-01.py
+++ noip/py2017-01.py
@@ -7,10 +7,10 @@ Created on Tue Dec  5 09:52:57 2017
 
 import math
 from operator import mod
-
+import time
 def func(a,b,c,d):
   if mod(c,a)==0 and mod(d,b)==0:
-    #print( c+d,c/a,d/b)
+    print( c+d,c/a,d/b)
     return 0
   elif(d-a)<0:
     print( "*",c+d,c/a,d/b)
@@ -18,15 +18,19 @@ def func(a,b,c,d):
   else:
     func(a,b,c+a,d-a)
     
-
+ 
 def main():
-  print("3,7")
-  for n in range(11,101,1):  
-    func(3,7,0,n)
+#  print("3,7")
+ # for n in range(11,101,1):  
+  #  func(3,7,0,n)
   
+  start=time.strftime('%H:%M:%S',time.localtime(time.time()))
   print("3,5")
-  for n in range(1,100001,1):  
+  #for n in range(1,1000001,1):  
+  for n in range(1,1000001,1):  
     func(3,5,0,n)
+  print(start)
+  print(time.strftime('%H:%M:%S',time.localtime(time.time())))
 
 if __name__ == '__main__':
     main()
