ae8508be7dd771d52b61cf24fe976aca68a38e45
ty-hongjy <hongjy1@163.com>
2017-12-10 21:08:06 +0800

    1210

--- noip/py2017-01_forever.py
+++ noip/py2017-01_forever.py
@@ -8,6 +8,8 @@ Created on Tue Dec  5 09:52:57 2017
 import math
 from operator import mod
 import time
+import datetime
+
 def func(a,b,c):
   d=c
   c=0
@@ -24,13 +26,31 @@ def func(a,b,c):
  
 def main():
   
-  start=time.strftime('%H:%M:%S',time.localtime(time.time()))
+#  start=time.strftime('%H:%M:%S',time.localtime(time.time()))
+#  startms=time.strftime('%H:%M:%S',time.localtime(time.time()))
+#  dt=datetime.datetime.now().microsecond
+#  t = time.time()
+#  nowTime1 = lambda:int(round(t * 1000))
+#  print (nowTime1());
+#  print(dt)              #毫秒级时间戳，基于lambda
+
+  begin = datetime.datetime.now()
   print("3,5")
-  #for n in range(1,1000001,1):  
-  for n in range(1,101,1):  
+  for n in range(1,10001,1):  
+  #for n in range(1,101,1):  
     func(3,5,n)
-  print(start)
-  print(time.strftime('%H:%M:%S',time.localtime(time.time())))
+#  print(start)
+#  print(time.strftime('%H:%M:%S',time.localtime(time.time())))
+#
+#  t = time.time()
+#  nowTime = lambda:int(round(t * 1000))
+  
+#  print ("T1:",nowTime1());              #毫秒级时间戳，基于lambda
+#  print ("T2",nowTime());   
+#  print(nowTime()-nowTime1())           #毫秒级时间戳，基于lambda
+  end = datetime.datetime.now()
+  k = end - begin
+  print("total_seconds:",k.total_seconds())
 
 if __name__ == '__main__':
     main()
