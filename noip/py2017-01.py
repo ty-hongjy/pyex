405ed37da3afbcae0be7a5bc5781fdd736aa1737
ty-hongjy <hongjy1@163.com>
2017-12-10 13:20:46 +0800

    1210

--- noip/py2017-01.py
+++ noip/py2017-01.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Dec  5 09:52:57 2017
 
-@author: Administrator
+@author: ty-hongjy
 """
 
 import math
@@ -10,21 +10,23 @@ from operator import mod
 
 def func(a,b,c,d):
   if mod(c,a)==0 and mod(d,b)==0:
-    print( c+d,c/a,d/b)
+    #print( c+d,c/a,d/b)
     return 0
   elif(d-a)<0:
-    print( c+d,c/a,d/b)
+    print( "*",c+d,c/a,d/b)
     return -1
   else:
     func(a,b,c+a,d-a)
     
 
 def main():
-  
+  print("3,7")
   for n in range(11,101,1):  
     func(3,7,0,n)
   
+  print("3,5")
+  for n in range(1,100001,1):  
+    func(3,5,0,n)
+
 if __name__ == '__main__':
     main()
-
-  
