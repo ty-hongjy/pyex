7907428353c00903a177d09b29dad71862b5de46
ty-hongjy <hongjy1@163.com>
2023-03-29 16:34:10 +0800

    2023-03-29

--- cn_uni_mooc-sklearn/traffic/Traffic_D.py
+++ cn_uni_mooc-sklearn/traffic/Traffic_D.py
@@ -15,18 +15,22 @@ import matplotlib.pyplot as plt
 def load_datasets(feature_paths, label_paths):
     feature = np.ndarray(shape=(0,7))
     label = np.ndarray(shape=(0,1))
+    df_l=None
     for file in feature_paths:
         df = pd.read_csv(file, delimiter=',', na_values='?')
+        df_l = df.copy()
         print("df size",len(df.columns))
         # df = pd.read_csv(file, delimiter=',', na_values='?', header=None)
-        # df['Date Occurred'] = pd.to_datetime(df['Date Occurred'])
-        df['Time Occurred'] = pd.to_datetime(df['Date Occurred'],df['Time Occurred'])
-        # df['Time Occurred'] = pd.concat(df['Date Occurred'],df['Time Occurred'])
-        print(df['Time Occurred'])
-        # df['Time Occurred'] = pd.to_numeric(['Time Occurred'],downcast=astype(int64))
-        
+        # df['Time Occurred'] = pd.to_datetime(df['Date Occurred'],df['Time Occurred'])
+        # print(df['Time Occurred'])
+        # df['MO Codes']= df['MO Codes'].astype('category')
+        df['Victim Sex']= df['Victim Sex'].astype('str')
+        df['Victim Sex']= df['Victim Sex'].astype('category')
+        print(df['Victim Sex'])
+        df.info()
         # df.drop([ 'Area ID','Area Name','Date Reported','Date Occurred', 'Crime Code Description',
         df.drop([ 'Area ID','Area Name','Date Reported','Date Occurred', 'Time Occurred','Crime Code Description',
+                # 'MO Codes','Victim Descent','Premise Description','Address','Cross Street'], 
                 'MO Codes','Victim Descent','Premise Description','Address','Cross Street','Victim Sex'], 
                  axis=1,inplace=True)
         # df.to_timestamp(['Date Occurred'])
@@ -42,23 +46,17 @@ def load_datasets(feature_paths, label_paths):
         df = imp.transform(df)
         feature = np.concatenate((feature, df))
 
-    for file in label_paths:
-        df = pd.read_csv(file, delimiter=',', na_values='?')
-        # df = pd.read_csv(file, delimiter=',', na_values='?', header=None)
-        # df2=df.take([4], axis=1)
-        # df2=df[4]
-        # df2=df.take([ 'Area ID'], axis=1)
-        df2=df.take([4], axis=1)
+    df2=df_l.take([4], axis=1)
         # df2=df.take([4], axis=1)
         # print(type(df))
         # print("df2 size",len(df.columns))
         # print(df)
         # print(type(df2))
         # df2.drop(axis=0, index=0, columns=None, inplace=False)
-        print('l df len',len(df2))
+    print('l df len',len(df2))
         # print("df2 size",len(df2.columns))
         # print(df2)
-        label = np.concatenate((label, df2))
+    label = np.concatenate((label, df2))
 
     label = np.ravel(label)
     return feature, label
@@ -71,6 +69,7 @@ def main():
     testfilePaths = ['Traffic_Collision_Data_from_2010_to_Today.csv']
     ''' 读入数据  '''
     x,y = load_datasets(trainfilePaths,testfilePaths)
+    # return
     x_train, x_test, y_train, y_test = \
     train_test_split(x, y, test_size=0.3, random_state=0,stratify=y)
     print('Start DT training')
